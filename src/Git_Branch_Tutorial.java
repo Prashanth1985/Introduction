cb8d4410901ba63c0407dddd868bf92405dba732
Prashanth <prashanthtrivedi@gmail.com>
2025-11-26 16:33:40 +0530

    git_branch_tutuorial

---
+++ src/Git_Branch_Tutorial.java
@@ -0,0 +1,23 @@
+
+public class Git_Branch_Tutorial {
+	
+/*
+ * A branch is like seperate workspace where you can make your own changes  without effecting main project 
+ * 
+ * commands
+ * 
+ * git branch<branchname> : to create the branch in repository 
+ * git branch : list of branches
+ * git checkout<branchname>/git switch <branchname> :-  to switch between branch
+ * git branch -m old name newname : to rename the branch 
+ * git push origin  -delete -branch <branchname>  to delete the branch from remote repository 
+ * 
+ * 
+ */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
