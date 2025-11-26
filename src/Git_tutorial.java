ee3efa257e40eeedb5d41eb9cca744548f6a6721
Prashanth <prashanthtrivedi@gmail.com>
2025-11-26 13:39:30 +0530

    git_tutorials

---
+++ src/Git_tutorial.java
@@ -0,0 +1,89 @@
+
+public class Git_tutorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	/*
+	 * GIT : it is know as popular distributed version control  system.
+	 * it is used for tracking code changes,tracking who made changes ,coding collabrations
+	 * 
+	 * Repository :- a folder where git track your project and its history 
+	 * 
+	 * Clone : make copy fo remote repository on your computer 
+	 * 
+	 * stage : Tell git which changes your to save next
+	 * 
+	 * commit :- save a snapshot of your staged changes
+	 * 
+	 * branch :0- work on different version or features at same time 
+	 * 
+	 * Merge : combine changes from different branches
+	 * 
+	 * Pull :- get the latest changes from remote repository 
+	 * 
+	 * Push :- suend your changes to remote repository 
+	 * 
+	 * git  config : to know user name and email id 
+	 * 
+	 * git status  :- i shows the current statu of your working directory and staging area 
+	 * 
+	 * Eg : -Tracked file and untracked file 
+	 * 
+	 * UnTracked file in your project folder that git is not yet tracking 
+	 * 
+	 * tracked file : is file that git is watching changes 
+	 * 
+	 * 
+	 * git commands 
+	 * 
+	 * git config --list : - to check the user name and password
+	 * 
+	 * touch text.txt/mkdir text.txt   to create file on folder 
+	 * 
+	 * git init : to initialize git on that folder 
+	 * 
+	 * .git : it is  hidden git  file   in respective folder 
+	 * 
+	 * ls -a : to display the hiddend folder 
+	 * 
+	 * git add<filename> : this command will add file/folder  from local repository to staging area 
+	 * 
+	 * git add --all : this command will add all folder in staging area 
+	 * 
+	 * git restore --staged <filename> : this command will remove file from staging area eg : git restore --staged src/Git_tutorial.java
+	 * 
+	 * git log : - it will display the log of commits and adding file to staging area 
+	 * 
+	 * git log --online : -it will display the log and commit line wise 
+	 * 
+	 * git remote -v  : to check the current repositories 
+	 * 
+	 * git remote  add origin <url>  ; it will add given url in repository 
+	 * 
+	 * git remote remove origin  : it will remove the repository url 
+	 * 
+	 * git remote set-url origin <url> : to change repository to another repository 
+	 * 
+	 * git tag<tagname> it will label or mark for specific commit 
+	 * git tag -d <name> : it will delete the tage
+	 * 
+
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+
+	}
+
+}
