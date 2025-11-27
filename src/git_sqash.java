e20ade965d1cb8b661ca7bfab9bfe41feb710db8
Prashanth <prashanthtrivedi@gmail.com>
2025-11-27 13:01:29 +0530

    stash

---
+++ src/git_sqash.java
@@ -0,0 +1,22 @@
+
+
+
+/*
+ * Git squash means combining multiple commits into a single commit.
+This is commonly used to clean up commit history before merging a feature branch.
+
+squash >>>1
+sqaash>>>>2
+sqassh>>>3
+
+
+
+ */
+public class git_sqash {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
